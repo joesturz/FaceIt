19544f1a37698a441213b7ccda49a256429c5262
Joe Sturzenegger <joesturz84@gmail.com>
2018-01-18 21:57:01 -0700

    created a simple app demostrating how to build a custom UIView

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Joe Sturzenegger on 1/18/18.
+//  Copyright © 2018 Joe Sturzenegger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+
+}
+
